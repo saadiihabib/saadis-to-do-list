4f780358a6326548b737774c693b9d0d4d867746
saadiihabib <saadulwahab1316@gmail.com>
2024-04-24 17:59:23 -0400

    pushing code to github
    a simple todo list application for beginners.

---
+++ index.ts
@@ -0,0 +1,28 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+let todoList = [];
+let conditions = true;
+console.log(chalk.red("\n \tWelcome to my to do list appliaction \n"));
+while (conditions) {
+    let addTasks = await inquirer.prompt([
+        {
+            name : "task",
+            type : "input",
+            message :chalk.green ("Enter your new task : "),
+        }
+    ]);
+    todoList.push(addTasks.task)
+    console.log(`${addTasks.task} task added to list successfully.`)
+
+    let addmoreTasks = await inquirer.prompt([
+        {
+        name : "addmore",
+        type : "confirm",
+        message : "would you like  to add another task to the list?",
+        default : "false",
+    }
+]);
+conditions = addmoreTasks.addmore;
+
+}
+console.log("your todo list udated successfully:" , todoList)
